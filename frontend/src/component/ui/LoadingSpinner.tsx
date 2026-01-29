b2c9575b22403a0370bf95714360838caf7e0258
razan-sameh <razansameh53@gmail.com>
2026-01-29 04:36:16 +0200

    update landing page

--- frontend/src/component/ui/LoadingSpinner.tsx
+++ frontend/src/component/ui/LoadingSpinner.tsx
@@ -3,7 +3,7 @@
 export default function LoadingSpinner() {
   return (
     <div className="flex items-center justify-center w-full h-[60vh]">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-primary"></div>
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-secondary"></div>
     </div>
   );
 }
